9419a9d33e29a078af884f45eb4c650f21b618f0
GlobalBooga <alex_anasta@hotmail.ca>
2024-02-05 21:57:48 -0500

    lvl1 update

--- Assets/Scripts/Gameplay/EncounterVolume.cs
+++ Assets/Scripts/Gameplay/EncounterVolume.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider), typeof(Checkpoint))]
 public class EncounterVolume : MonoBehaviour
 {
     [SerializeField] List<GameObject> barriers;
