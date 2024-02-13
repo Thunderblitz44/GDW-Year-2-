6af7ed655e69af37c9d642422f271e25062b6d5b
Thunderblitz44 <117412991+Thunderblitz44@users.noreply.github.com>
2024-02-13 18:22:17 -0500

    bbb

--- Assets/Prefabs/Golem Miniboss/Golem Boss Scripts/golemIkScript.cs
+++ Assets/Prefabs/Golem Miniboss/Golem Boss Scripts/golemIkScript.cs
@@ -30,7 +30,7 @@ public class golemIkScript : MonoBehaviour
     private void InitializeGolem()
     {
       
-        //footSpacing = transform.localPosition.x;
+        footSpacing = transform.localPosition.x;
         currentPosition = newPosition = oldPosition = transform.position;
         currentNormal = newNormal = oldNormal = transform.up;
         lerp = 1f;
@@ -48,7 +48,7 @@ public class golemIkScript : MonoBehaviour
         transform.position = currentPosition;
         transform.up = currentNormal;
 
-        if (Physics.Raycast(body.position + (-body.right * footSpacing), -body.parent.up, out RaycastHit info, 30f, terrainLayer.value))
+        if (Physics.Raycast(body.position + (-body.right * footSpacing), -body.up, out RaycastHit info, 30f, terrainLayer.value))
         {
            
             HandleStep(info);
