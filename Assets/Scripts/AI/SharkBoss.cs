ebb2e20242e0cc565314b9d4058cae9d66d9b518
GlobalBooga <alex_anasta@hotmail.ca>
2024-02-17 11:02:59 -0500

    mini insignificant update

---
+++ Assets/Scripts/AI/SharkBoss.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SharkBoss : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        //EnableAI();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+
+    }
+   
+    
+}
