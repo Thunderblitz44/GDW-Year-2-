565b11b605991aefc6c6948ab34694a5b2cb2d77
Thunderblitz44 <117412991+Thunderblitz44@users.noreply.github.com>
2024-03-14 21:59:19 -0400

    Health spirit + Lightning

--- Assets/Scripts/Gameplay/HeathPickup.cs
+++ Assets/Scripts/Gameplay/HeathPickup.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.VFX;
 public class HeathPickup : MonoBehaviour
 {
     [SerializeField] int healAmount = 50;
-
+    [SerializeField] private VisualEffect HealthEffect;
     private void Update()
     {
-        transform.rotation = Quaternion.LookRotation(StaticUtilities.FlatDirection(transform.position - LevelManager.PlayerTransform.position, Vector3.up));
+      //  transform.rotation = Quaternion.LookRotation(StaticUtilities.FlatDirection(transform.position - LevelManager.PlayerTransform.position, Vector3.up));
     }
 
     private void OnTriggerEnter(Collider other)
     {
         StaticUtilities.TryToDamage(other.transform.parent.gameObject, -healAmount);
+        HealthEffect.SendEvent("Pickup");
+   Invoke("DelayedDisable", 3f);
+    }
+
+  public void DelayedDisable()
+    {
         gameObject.SetActive(false);
     }
 }
